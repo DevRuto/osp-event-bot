993615cb7001ed3ba540db3126382fd78b5104d0
Ruto <ruto@protonmail.ch>
2025-03-25 16:07:56 -0400

    Add deploy_commands script

---
+++ deploy_commands.js
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import { REST, Routes } from 'discord.js';
+import fs from 'fs/promises';
+import path from 'path';
+
+const { DISCORD_TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+
+const commands = [];
+const baseDir = import.meta.dirname;
+const commandFiles = await fs.opendir(path.join(baseDir, './src/commands'), { recursive: true });
+for await (const commandFile of commandFiles) {
+  if (commandFile.isFile() && commandFile.name.endsWith('.js')) {
+    const relativeFile = `./${path.relative(baseDir, commandFile.parentPath)}/${commandFile.name}`;
+    const command = await import(relativeFile);
+    commands.push(command.data.toJSON());
+    console.log(`Loaded command: ${command.data.name} from ${relativeFile}`);
+  }
+}
+
+const rest = new REST().setToken(DISCORD_TOKEN);
+const data = await rest.put(
+  // Remove guild id to register global commands
+  Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+  { body: commands }
+);
+console.log(`Successfully reloaded ${data.length} application (/) commands.`);
