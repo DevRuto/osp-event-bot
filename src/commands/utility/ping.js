e7af46a9b3ec24a12d7bd17dae5c89a899f93c28
Ruto <ruto@protonmail.ch>
2025-03-25 20:21:09 -0400

    Add ESLint JSDoc

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,7 +1,10 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!');
 
+/**
+ * @param {ChatInputCommandInteraction} interaction
+ */
 export async function execute(interaction) {
   console.log('Pong!');
   await interaction.reply('Pong!');
