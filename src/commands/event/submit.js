b2b2df392e487662459a76f292e78d422e25fcca
Ruto <ruto@protonmail.ch>
2025-03-25 20:18:38 -0400

    Add submit command

---
+++ src/commands/event/submit.js
@@ -0,0 +1,33 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('submit')
+  .setDescription('Submit an item to the event')
+  .addStringOption((option) =>
+    option.setName('name').setDescription('The name of the item').setRequired(true)
+  )
+  .addStringOption((option) =>
+    option.setName('value').setDescription('The value of the item').setRequired(true)
+  )
+  .addStringOption((option) =>
+    option.setName('image').setDescription('The image link to the item').setRequired(false)
+  )
+  .addAttachmentOption((option) =>
+    option.setName('attachment').setDescription('Attached image of item').setRequired(false)
+  );
+
+export async function execute(interaction) {
+  const name = interaction.options.getString('name');
+  const value = interaction.options.getString('value');
+  const image = interaction.options.getString('image');
+  const attachment = interaction.options.getAttachment('attachment');
+
+  const proof = image || attachment?.url;
+  if (!proof) {
+    await interaction.reply('Please provide an image or attachment as proof of the item.');
+    return;
+  }
+
+  console.log(`Item submitted: ${name} - ${value}`);
+  await interaction.reply(`Item submitted: ${name} - ${value} - Proof type: ${proof}`);
+}
