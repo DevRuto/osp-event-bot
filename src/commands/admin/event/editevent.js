2758155dc618fa387b72d1fe6c267b46061d6277
Ruto <20764435+DevRuto@users.noreply.github.com>
2025-04-14 21:22:59 -0400

    Add create/edit event commands

---
+++ src/commands/admin/event/editevent.js
@@ -0,0 +1,108 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { ConfigService } from '#services/configService.js';
+import logger from '#utils/logger.js';
+import { EventService } from '#services/eventService.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('edit-event')
+  .setDescription('Edit an event')
+  .addStringOption((option) =>
+    option
+      .setName('event')
+      .setDescription('Select the event to edit')
+      .setRequired(true)
+      .setAutocomplete(true)
+  )
+  .addStringOption((option) =>
+    option.setName('name').setDescription('The name of the event').setRequired(false)
+  )
+  .addStringOption((option) =>
+    option.setName('description').setDescription('The description of the event').setRequired(false)
+  )
+  .addStringOption((option) =>
+    option.setName('startdate').setDescription('The start date of the event').setRequired(false)
+  )
+  .addStringOption((option) =>
+    option.setName('enddate').setDescription('The end date of the event').setRequired(false)
+  )
+  .addStringOption((option) =>
+    option
+      .setName('status')
+      .setDescription('The status of the event')
+      .setRequired(false)
+      .addChoices(
+        { name: 'Planning', value: 'PLANNING' },
+        { name: 'Ongoing', value: 'ONGOING' },
+        { name: 'Completed', value: 'COMPLETED' },
+        { name: 'Canceled', value: 'CANCELLED' }
+      )
+  );
+
+/**
+ * @param {ChatInputCommandInteraction} interaction
+ */
+export async function execute(interaction) {
+  if (!interaction.member.permissions.has('Administrator')) {
+    const customRole = await ConfigService.getAdminRole(interaction.guildId);
+    if (customRole) {
+      const member = await interaction.guild.members.fetch(interaction.user.id);
+      if (!member.roles.cache.has(customRole)) {
+        await interaction.reply('You do not have permission to set the approval channel.');
+        return;
+      }
+    } else {
+      await interaction.reply('You do not have permission to set the approval channel.');
+      return;
+    }
+  }
+
+  const eventId = interaction.options.getString('event');
+  const name = interaction.options.getString('name');
+  const description = interaction.options.getString('description');
+  const startDate = interaction.options.getString('startdate');
+  const endDate = interaction.options.getString('enddate');
+  const status = interaction.options.getString('status');
+
+  if (!name && !description && !startDate && !endDate && !status) {
+    await interaction.reply('Please provide at least one field to edit.');
+    return;
+  }
+  // validate datetime format
+  const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
+  if (startDate && !dateFormat.test(startDate)) {
+    await interaction.reply('Invalid start date format. Please use ISO 8601 format.');
+    return;
+  }
+  if (endDate && !dateFormat.test(endDate)) {
+    await interaction.reply('Invalid end date format. Please use ISO 8601 format.');
+    return;
+  }
+
+  try {
+    const updatedEvent = await EventService.updateEvent(
+      eventId,
+      name,
+      description,
+      startDate,
+      endDate,
+      status
+    );
+    await interaction.reply(`Event "${updatedEvent.name}" edited successfully!`);
+    logger.info(
+      `Event "${updatedEvent.name}" (${updatedEvent.id}) edited successfully by ${interaction.user.tag}`
+    );
+  } catch (error) {
+    await interaction.reply('An error occurred while editing the event. Please try again.');
+  }
+}
+
+/**
+ * @param {ChatInputCommandInteraction} interaction
+ */
+export async function autocomplete(interaction) {
+  const focusedValue = interaction.options.getFocused();
+  const events = await EventService.getEvents(interaction.guildId);
+  const filtered = events.filter((event) => event.name.startsWith(focusedValue));
+  const choices = filtered.map((event) => event).slice(0, 25);
+  await interaction.respond(choices.map((choice) => ({ name: choice.name, value: choice.id })));
+}
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,20 +1,42 @@
-import { Events, MessageFlags } from 'discord.js';
+import { Events, MessageFlags, ChatInputCommandInteraction } from 'discord.js';
 import logger from '#utils/logger.js';
 
 export const name = Events.InteractionCreate;
+/**
+ * @param {ChatInputCommandInteraction} interaction
+ */
 export async function execute(interaction) {
-  if (!interaction.isChatInputCommand()) return;
+  if (interaction.isChatInputCommand()) {
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) return;
 
-  const command = interaction.client.commands.get(interaction.commandName);
-  if (!command) return;
+    try {
+      if (interaction.isAutocomplete()) {
+        await command.autocomplete(interaction);
+      } else {
+        await command.execute(interaction);
+      }
+    } catch (error) {
+      logger.error(error);
+      await interaction.reply({
+        content: 'There was an error while executing this command!',
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
 
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    logger.error(error);
-    await interaction.reply({
-      content: 'There was an error while executing this command!',
-      flags: MessageFlags.Ephemeral,
-    });
+  if (interaction.isAutocomplete()) {
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+      await command.autocomplete(interaction);
+    } catch (error) {
+      logger.error(error);
+      await interaction.reply({
+        content: 'There was an error while executing this command!',
+        flags: MessageFlags.Ephemeral,
+      });
+    }
   }
 }
