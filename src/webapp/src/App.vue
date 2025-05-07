ea9f6c9385ef47a8b85fad27d3df8b71bcb296f6
Ruto <20764435+DevRuto@users.noreply.github.com>
2025-05-07 19:29:00 -0400

    Change to Domain

--- src/webapp/src/App.vue
+++ src/webapp/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="bg-gray-800 text-white px-6 py-4 flex justify-between items-center">
-      <div class="text-lg font-semibold">Ruto's Event</div>
+      <div class="text-lg font-semibold">Ruto's Domain</div>
       <div class="space-x-4">
         <RouterLink to="/" class="hover:text-gray-300">Home</RouterLink>
         <RouterLink to="/participants" class="hover:text-gray-300">Participants</RouterLink>
