a9ad3a851dd3de81601a7dd1518e95ae3451c00e
Ruto <20764435+DevRuto@users.noreply.github.com>
2025-05-08 17:01:31 -0400

    Set file log to root

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,7 +2,7 @@ import pino from 'pino';
 import fs from 'fs';
 import path from 'path';
 
-const logDir = path.join(import.meta.dirname, 'logs');
+const logDir = path.join(import.meta.dirname, '../../', 'logs');
 if (!fs.existsSync(logDir)) fs.mkdirSync(logDir);
 
 const prettyTransport = pino.transport({
