2b777d9c8a5f94ea9b988c7eda3b63549c8369f6
Winston <devruto@gmail.com>
2025-05-20 19:24:08 -0400

    Add Hiscore logs

---
+++ hiscore_logs/hiscore.d.ts
@@ -0,0 +1,133 @@
+export interface PlayerHiscore {
+    skills: Skills;
+    minigames: Minigames;
+    bosses: Bosses;
+  }
+  export interface Skills {
+    overall: Skill;
+    attack: Skill;
+    defence: Skill;
+    strength: Skill;
+    hitpoints: Skill;
+    ranged: Skill;
+    prayer: Skill;
+    magic: Skill;
+    cooking: Skill;
+    woodcutting: Skill;
+    fletching: Skill;
+    fishing: Skill;
+    firemaking: Skill;
+    crafting: Skill;
+    smithing: Skill;
+    mining: Skill;
+    herblore: Skill;
+    agility: Skill;
+    thieving: Skill;
+    slayer: Skill;
+    farming: Skill;
+    runecrafting: Skill;
+    hunter: Skill;
+    construction: Skill;
+  }
+  export interface Skill {
+    rank: number;
+    level: number;
+    xp: number;
+  }
+  export interface Minigames {
+    leaguePoints: Minigame;
+    deadmanPoints: Minigame;
+    bountyHunter: Minigame;
+    bountyHunterRogues: Minigame;
+    bountyHunterLegacy: Minigame;
+    bountyHunterRoguesLegacy: Minigame;
+    clueScrollsAll: Minigame;
+    clueScrollsBeginner: Minigame;
+    clueScrollsEasy: Minigame;
+    clueScrollsMedium: Minigame;
+    clueScrollsHard: Minigame;
+    clueScrollsElite: Minigame;
+    clueScrollsMaster: Minigame;
+    lms: Minigame;
+    pvpArena: Minigame;
+    soulWarsZeal: Minigame;
+    riftsClosed: Minigame;
+    colosseumGlory: Minigame;
+    collectionsLogged: Minigame;
+  }
+  export interface Minigame {
+    rank: number;
+    score: number;
+  }
+  export interface Bosses {
+    abyssalSire: Boss;
+    alchemicalHydra: Boss;
+    amoxliatl: Boss;
+    araxxor: Boss;
+    artio: Boss;
+    barrowsChests: Boss;
+    bryophyta: Boss;
+    callisto: Boss;
+    calvarion: Boss;
+    cerberus: Boss;
+    chambersOfXeric: Boss;
+    chambersOfXericChallengeMode: Boss;
+    chaosElemental: Boss;
+    chaosFanatic: Boss;
+    commanderZilyana: Boss;
+    corporealBeast: Boss;
+    crazyArchaeologist: Boss;
+    dagannothPrime: Boss;
+    dagannothRex: Boss;
+    dagannothSupreme: Boss;
+    derangedArchaeologist: Boss;
+    dukeSucellus: Boss;
+    generalGraardor: Boss;
+    giantMole: Boss;
+    grotesqueGuardians: Boss;
+    hespori: Boss;
+    kalphiteQueen: Boss;
+    kingBlackDragon: Boss;
+    kraken: Boss;
+    krilTsutsaroth: Boss;
+    lunarChests: Boss;
+    mimic: Boss;
+    nex: Boss;
+    nightmare: Boss;
+    phosanisNightmare: Boss;
+    obor: Boss;
+    phantomMuspah: Boss;
+    sarachnis: Boss;
+    scorpia: Boss;
+    scurrius: Boss;
+    skotizo: Boss;
+    solHeredit: Boss;
+    spindel: Boss;
+    tempoross: Boss;
+    gauntlet: Boss;
+    corruptedGauntlet: Boss;
+    hueycoatl: Boss;
+    leviathan: Boss;
+    royalTitans: Boss;
+    whisperer: Boss;
+    theatreOfBlood: Boss;
+    theatreOfBloodHardMode: Boss;
+    thermonuclearSmokeDevil: Boss;
+    tombsOfAmascut: Boss;
+    tombsOfAmascutExpertMode: Boss;
+    tzKalZuk: Boss;
+    tzTokJad: Boss;
+    vardorvis: Boss;
+    venenatis: Boss;
+    vetion: Boss;
+    vorkath: Boss;
+    wintertodt: Boss;
+    yama: Boss;
+    zalcano: Boss;
+    zulrah: Boss;
+  }
+  export interface Boss {
+    rank: number;
+    kills: number;
+  }
+  
